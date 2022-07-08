6421c0c94b06dd73ba76d89d63693362edde9040
Matheus <mfabryny@minsait.com>
2022-07-07 23:40:16 -0300

    conectando com API, utilizando token

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ListComponent from './ListComponent';
 
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listName={'Minha lista'} />
-            <ListComponent listName={'Minha outra lista'} />
-        </div>
-    )
+export default class UserLists extends React.Component{
+    state = { listas: [], loading: true }
+
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 5655c9c78deac598e011a48a159cb467d7d12d6a'
+
+        var url = "http://127.0.0.1:8000/Lista/";
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data) 
+        this.setState({listas: data, loading: false})
+    }
+
+    render()
+    {
+        const listasApi = this.state.listas;
+        return (
+            <div>
+                {listasApi.map(lista => <ListComponent key={lista.id} listName={lista.nome} /> )}
+            </div>
+        )
+    }
 }
